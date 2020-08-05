28dd0f41e4dcc4b621171fcfc10058a06eb0e394
Josejulio Martínez <jmartine@redhat.com>
2020-08-05 10:14:37 -0500

    Additional checks  - Check prod schema on prod or prod-stable branches  - Only call deploy when pushing to a branch, not on PRs  - Remove branch check inside the schema check script

--- .github/scripts/schema-check.sh
+++ .github/scripts/schema-check.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" = "prod" ]
-then
-    curl https://cloud.redhat.com/api/notifications/v1.0/openapi.json -o ./utils/openapi-generator/openapi.json
-    yarn schema:generate && yarn schema:clean
-    [[ -z $(git status --porcelain) ]] ||
-    (
-      git status &&
-      git diff &&
-      echo "----------------------------------------------------------------" &&
-      echo "  openapi.json types or actions are out of sync aborting build. " &&
-      echo "----------------------------------------------------------------" &&
-      false
-    )
-fi
+curl https://cloud.redhat.com/api/notifications/v1.0/openapi.json -o ./utils/openapi-generator/openapi.json
+yarn schema:generate && yarn schema:clean
+[[ -z $(git status --porcelain) ]] ||
+(
+  git status &&
+  git diff &&
+  echo "----------------------------------------------------------------" &&
+  echo "  openapi.json types or actions are out of sync aborting build. " &&
+  echo "----------------------------------------------------------------" &&
+  false
+)
