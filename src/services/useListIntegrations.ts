48ff8fc3a72dbc4a47fc3d29083e2484cf3554ba
Josejulio Martínez <jmartine@redhat.com>
2020-09-30 16:20:57 -0500

    Allows undefined page on listIntegrations

--- src/services/useListIntegrations.ts
+++ src/services/useListIntegrations.ts
@@ -6,10 +6,10 @@ import { Page, useTransformQueryResponse } from '@redhat-cloud-services/insights
 import { useQuery } from 'react-fetching-library';
 import { toIntegrations } from '../types/adapters/IntegrationAdapter';
 
-export const listIntegrationsActionCreator = (pager: Page) => {
+export const listIntegrationsActionCreator = (pager?: Page) => {
     return actionGetEndpoints({
-        pageNumber: pager.index,
-        pageSize: pager.size
+        pageNumber: pager?.index,
+        pageSize: pager?.size
     });
 };
 
@@ -25,7 +25,7 @@ const decoder = (payload?: GetEndpointsPayload) => {
     return payload;
 };
 
-export const useListIntegrationsQuery = (pager: Page) => useTransformQueryResponse(
+export const useListIntegrationsQuery = (pager?: Page) => useTransformQueryResponse(
     useQuery(listIntegrationsActionCreator(pager)),
     decoder
 );
