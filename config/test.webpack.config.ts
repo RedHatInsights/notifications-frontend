03329d83be099e17040cecb52a1c20f10397ffbf
Josejulio Martínez <jmartine@redhat.com>
2021-02-10 07:54:03 -0600

    Upgrade to frontend-components v3+ (#75)
    * Upgrade to frontend-components v3+
     - Upgrades webpack to 5
     - Includes a typescript extension to avoid including whole
       react-token & react-icons
    
    * Lock dependencies

--- config/test.webpack.config.ts
+++ config/test.webpack.config.ts
@@ -1,9 +1,14 @@
 import config from '@redhat-cloud-services/frontend-components-config';
 import { resolve } from 'path';
 
+import { updateTsLoaderRule } from './TsLoaderCustomTransformers';
+
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../')
 });
+
+// Upgrade module.rules to include transformers rules
+updateTsLoaderRule(webpackConfig.module.rules);
 
 module.exports = {
     ...webpackConfig,
