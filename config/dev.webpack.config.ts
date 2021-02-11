d92d84282c0d5f059816e80326bd84501fd91a85
Josejulio Martínez <jmartine@redhat.com>
2021-02-10 18:04:29 -0600

    Revert "Upgrade to frontend-components v3+ (#75)" (#77)
    This reverts commit 03329d83be099e17040cecb52a1c20f10397ffbf.

--- config/dev.webpack.config.ts
+++ config/dev.webpack.config.ts
@@ -1,26 +1,11 @@
 import config from '@redhat-cloud-services/frontend-components-config';
 import { resolve } from 'path';
 
-import { updateTsLoaderRule } from './TsLoaderCustomTransformers';
-// import * as webpack from 'webpack';
-// import ProfilingPlugin = webpack.debug.ProfilingPlugin;
-// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../'),
     debug: true,
-    https: true,
-    useFileHash: false
+    https: true
 });
-
-// Upgrade module.rules to include transformers rules
-updateTsLoaderRule(webpackConfig.module.rules);
-
-webpackConfig.devtool = 'eval-cheap-module-source-map';
-// Used for profiling the build, generates an event.js that can be loaded in google chrome performance tab
-// plugins.push(new ProfilingPlugin());
-// Checks final bundle sizes and what elements are generated
-// plugins.push(new BundleAnalyzerPlugin());
 
 module.exports = {
     ...webpackConfig,
