01823b430bac020549964f4fd5ff43d2f7221d7b
Josejulio Martínez <jmartine@redhat.com>
2021-02-12 15:12:04 -0600

    Optimize build speed

--- config/prod.webpack.config.ts
+++ config/prod.webpack.config.ts
@@ -1,9 +1,12 @@
 import config from '@redhat-cloud-services/frontend-components-config';
 import { resolve } from 'path';
+import { updateTsLoaderRule } from './common.webpack.config';
 
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../')
 });
+
+updateTsLoaderRule(webpackConfig.module.rules);
 
 module.exports = {
     ...webpackConfig,
