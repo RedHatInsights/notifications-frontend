a0470bb62609ac66589bd5864fb1441e29f3c39d
Josejulio Martínez <jmartine@redhat.com>
2021-07-20 13:27:11 -0500

    NOTIF-251 Updated form modal to use a table for the actions

--- src/components/Notifications/BehaviorGroup/BehaviorGroupForm.tsx
+++ src/components/Notifications/BehaviorGroup/BehaviorGroupForm.tsx
@@ -1,185 +1,27 @@
-import { Button, ButtonVariant, Form, FormGroup, Grid, GridItem, Split, SplitItem } from '@patternfly/react-core';
-import { MinusCircleIcon, PlusCircleIcon } from '@patternfly/react-icons';
+import { Form, Grid, GridItem } from '@patternfly/react-core';
 import { global_spacer_md } from '@patternfly/react-tokens';
 import { FormTextInput, OuiaComponentProps, ouiaIdConcat } from '@redhat-cloud-services/insights-common-typescript';
-import { FieldArray, FieldArrayRenderProps, FormikProps, useField } from 'formik';
+import { FieldArray } from 'formik';
 import * as React from 'react';
-import { DeepPartial } from 'ts-essentials';
 import { style } from 'typestyle';
 
-import { IntegrationType, UserIntegrationType } from '../../../types/Integration';
-import { Action, BehaviorGroup, IntegrationRef, NewBehaviorGroup, NotificationType } from '../../../types/Notification';
+import {  UserIntegrationType } from '../../../types/Integration';
+import { BehaviorGroup, IntegrationRef } from '../../../types/Notification';
 import { getOuiaProps } from '../../../utils/getOuiaProps';
-import { RecipientForm } from '../EditableActionRow/RecipientForm';
-import { useEditableActionRow } from '../EditableActionRow/useEditableActionRow';
-import { ActionTypeahead } from '../Form/ActionTypeahead';
+import { BehaviorGroupFormActionsTable } from './BehaviorGroupFormActionsTable';
 
-interface EditBehaviorGroupProps extends OuiaComponentProps {
+export interface EditBehaviorGroupProps extends OuiaComponentProps {
     getRecipients: (search: string) => Promise<Array<string>>;
     getIntegrations: (type: UserIntegrationType, search: string) => Promise<Array<IntegrationRef>>;
     behaviorGroup?: Partial<BehaviorGroup>;
 }
 
-type FormType = BehaviorGroup | NewBehaviorGroup;
-
-type ActionsArraysProps = FieldArrayRenderProps & Pick<EditBehaviorGroupProps, 'getRecipients' | 'getIntegrations'>  & {
-    form: FormikProps<FormType>;
-}
-
-type ActionRowProps = {
-    selectedNotifications: ReadonlyArray<NotificationType>;
-    action: Action;
-    path: string;
-    getRecipients: (search: string) => Promise<Array<string>>;
-    getIntegrations: (type: UserIntegrationType, search: string) => Promise<Array<IntegrationRef>>;
-    onRemove?: () => void;
-};
-
-const alignLeftClassName = style({
-    textAlign: 'left',
-    paddingLeft: 0
-});
-
 const subtitleClassName = style({
     paddingBottom: global_spacer_md.var
 });
 
-const ActionRow: React.FunctionComponent<ActionRowProps> = props => {
-    const {
-        recipientSelected,
-        recipientOnClear,
-        integrationSelected,
-        actionSelected
-    } = useEditableActionRow(props.path);
-
-    const [
-        field,
-        meta
-    ] = useField(props.path);
-
-    const error = React.useMemo(() => {
-        if (typeof meta.error === 'string') {
-            return meta.error;
-        } else if ((meta.error as any)?.integration?.id) {
-            return 'Select a recipient for this integration';
-        }
-
-        return undefined;
-    }, [ meta.error ]);
-
-    return (
-        <>
-            <GridItem span={ 6 }>
-                <ActionTypeahead
-                    selectedNotifications={ props.selectedNotifications }
-                    action={ props.action }
-                    onSelected={ actionSelected }
-                />
-            </GridItem>
-            <GridItem span={ 6 }>
-                <Split>
-                    <SplitItem isFilled>
-                        <RecipientForm
-                            getRecipients={ props.getRecipients }
-                            getIntegrations={ props.getIntegrations }
-                            recipientSelected={ recipientSelected }
-                            recipientOnClear={ recipientOnClear }
-                            integrationSelected={ integrationSelected }
-                            action={ props.action }
-                        />
-                    </SplitItem>
-                    { props.onRemove && (
-                        <SplitItem>
-                            <Button variant={ ButtonVariant.plain } onClick={ props.onRemove }>
-                                <MinusCircleIcon />
-                            </Button>
-                        </SplitItem>
-                    ) }
-                </Split>
-            </GridItem>
-            { error && <GridItem span={ 12 }><FormGroup fieldId={ `${field.name}` } helperTextInvalid={ error } validated="error" /></GridItem> }
-        </>
-    );
-};
-
-const ActionsArray: React.FunctionComponent<ActionsArraysProps> = props => {
-    const { values, isSubmitting } = props.form;
-    const actions = React.useMemo(() => values.actions ?? [] as Array<Action>, [ values ]);
-
-    const getIntegrations = React.useCallback(async (type: UserIntegrationType, search: string) => {
-        const rawGetIntegrations = props.getIntegrations;
-        const integrations = await rawGetIntegrations(type, search);
-        const existingIntegrations = actions.map(a => a.integrationId);
-        return integrations.filter(integration => !existingIntegrations.includes(integration.id));
-    }, [ props.getIntegrations, actions ]);
-
-    const selectedNotifications = React.useMemo<ReadonlyArray<NotificationType>>(
-        () => new Array(...new Set<NotificationType>(actions.map(a => a.type))),
-        [ actions ]
-    );
-
-    const addAction = React.useCallback(() => {
-        const push = props.push;
-        let newAction: DeepPartial<Action>;
-        if (selectedNotifications.includes(NotificationType.EMAIL_SUBSCRIPTION)) {
-            newAction = {
-                type: NotificationType.INTEGRATION,
-                integration: {
-                    type: IntegrationType.WEBHOOK
-                }
-            };
-        } else {
-            newAction = {
-                type: NotificationType.EMAIL_SUBSCRIPTION,
-                integrationId: '',
-                recipient: []
-            };
-        }
-
-        push(newAction);
-    }, [ props.push, selectedNotifications ]);
-
-    React.useEffect(() => {
-        if (actions.length === 0) {
-            addAction();
-        }
-    }, [ actions, addAction ]);
-
-    return (
-        <>
-            <GridItem span={ 6 }>
-                <b>Actions</b>
-            </GridItem>
-            <GridItem span={ 6 }>
-                <b>Recipient</b>
-            </GridItem>
-            { actions.map((action, index) => (
-                <ActionRow
-                    key={ `${index}-${action.integrationId}` }
-                    selectedNotifications={ selectedNotifications }
-                    action={ action }
-                    getIntegrations={ getIntegrations }
-                    getRecipients={ props.getRecipients }
-                    path={ `actions.${index}` }
-                    onRemove={ actions.length > 1 ? props.handleRemove(index) : undefined }
-                />
-            )) }
-            <GridItem span={ 6 }>
-                <Button
-                    className={ alignLeftClassName }
-                    variant={ ButtonVariant.link }
-                    icon={ <PlusCircleIcon /> }
-                    onClick={ addAction }
-                    isDisabled={ isSubmitting }
-                >
-                    Add action
-                </Button>
-            </GridItem>
-        </>
-    );
-};
-
 export const EditBehaviorGroupForm: React.FunctionComponent<EditBehaviorGroupProps> = props => {
+
     return (
         <div { ... getOuiaProps('Notifications/BehaviorGroupForm', props) }>
             <div className={ subtitleClassName }>Enter a name and add actions for your new group.</div>
@@ -194,11 +36,15 @@ export const EditBehaviorGroupForm: React.FunctionComponent<EditBehaviorGroupPro
                         />
                     </GridItem>
                     <FieldArray name="actions">
-                        { helpers => <ActionsArray
-                            { ...helpers }
-                            getRecipients={ props.getRecipients }
-                            getIntegrations={ props.getIntegrations }
-                        /> }
+                        { helpers => (
+                            <>
+                                <BehaviorGroupFormActionsTable
+                                    { ...helpers }
+                                    getRecipients={ props.getRecipients }
+                                    getIntegrations={ props.getIntegrations }
+                                />
+                            </>
+                        ) }
                     </FieldArray>
                 </Grid>
             </Form>
--- src/components/Notifications/EditableActionRow/RecipientForm.tsx
+++ src/components/Notifications/EditableActionRow/RecipientForm.tsx
@@ -1,38 +1,57 @@
+import { FormHelperText } from '@patternfly/react-core';
+import { ExclamationCircleIcon } from '@patternfly/react-icons';
 import * as React from 'react';
 
 import { IntegrationType, UserIntegrationType } from '../../../types/Integration';
 import { Action, IntegrationRef, NotificationType } from '../../../types/Notification';
+import { UseBehaviorGroupActionHandlers } from '../BehaviorGroup/useBehaviorGroupActionHandlers';
 import { IntegrationRecipientTypeahead } from '../Form/IntegrationRecipientTypeahead';
 import { RecipientTypeahead } from '../Form/RecipientTypeahead';
-import { useEditableActionRow } from './useEditableActionRow';
 
 interface RecipientFormProps {
     action: Action;
-    integrationSelected: ReturnType<typeof useEditableActionRow>['integrationSelected'];
-    recipientSelected: ReturnType<typeof useEditableActionRow>['recipientSelected'];
-    recipientOnClear: ReturnType<typeof useEditableActionRow>['recipientOnClear'];
+    integrationSelected: ReturnType<UseBehaviorGroupActionHandlers['handleIntegrationSelected']>;
+    recipientSelected: ReturnType<UseBehaviorGroupActionHandlers['handleRecipientSelected']>;
+    recipientOnClear: ReturnType<UseBehaviorGroupActionHandlers['handleRecipientOnClear']>;
     getRecipients: (search: string) => Promise<Array<string>>;
     getIntegrations: (type: UserIntegrationType, search: string) => Promise<Array<IntegrationRef>>;
+    onOpenChange?: (isOpen: boolean) => void;
+    error?: string;
 }
 
 export const RecipientForm: React.FunctionComponent<RecipientFormProps> = props => {
+    let recipient: React.ReactNode;
+
     if (props.action.type === NotificationType.INTEGRATION) {
-        return (
+        recipient = (
             <IntegrationRecipientTypeahead
                 onSelected={ props.integrationSelected }
                 integrationType={ props.action.integration?.type ?? IntegrationType.WEBHOOK }
                 selected={ props.action.integration }
                 getIntegrations={ props.getIntegrations }
+                onOpenChange={ props.onOpenChange }
+            />
+        );
+    } else {
+        recipient = (
+            <RecipientTypeahead
+                onSelected={ props.recipientSelected }
+                selected={ props.action.recipient }
+                getRecipients={ props.getRecipients }
+                onClear={ props.recipientOnClear }
+                onOpenChange={ props.onOpenChange }
             />
         );
     }
 
     return (
-        <RecipientTypeahead
-            onSelected={ props.recipientSelected }
-            selected={ props.action.recipient }
-            getRecipients={ props.getRecipients }
-            onClear={ props.recipientOnClear }
-        />
+        <>
+            { recipient }
+            { props.error && (
+                <FormHelperText isError icon={ <ExclamationCircleIcon /> } isHidden={ !props.error }>
+                    { props.error }
+                </FormHelperText>
+            ) }
+        </>
     );
 };
