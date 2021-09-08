b6eb628e6f71c0fe37bf965ed213114bbecc84a0
Josejulio Martínez <jmartine@redhat.com>
2021-09-08 15:46:03 -0500

    Fixes mock

--- config/setupTests.ts
+++ config/setupTests.ts
@@ -1,12 +1,15 @@
-import { getInsights } from '@redhat-cloud-services/insights-common-typescript';
 import { mockInsights } from 'insights-common-typescript-dev';
 import React from 'react';
 
 import { mockResizeObserver } from './testutils/ResizeObserverMock';
 
 declare const window: any;
+declare const insights: any;
 window.React = React;
 mockInsights();
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-getInsights().chrome.on = jest.fn(() => () => {});
+insights.chrome.on = jest.fn(() => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    return () => {};
+});
+
 mockResizeObserver();
