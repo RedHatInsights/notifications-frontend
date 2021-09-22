d2e8b1978d8c7520bfdf0372efbedf1369ffc2f0
Josejulio Martínez <jmartine@redhat.com>
2021-09-22 17:41:13 -0500

    NOTIF-327 Remove schema check for prod

--- .travis/schema-check.sh
+++ .travis/schema-check.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" = "prod" ] || [ "${TRAVIS_BRANCH}" = "prod-stable" ]
-then
-    yarn schema:integrations:generate -i https://cloud.redhat.com/api/integrations/v1.0/openapi.json
-    yarn schema:notifications:generate -i https://cloud.redhat.com/api/notifications/v1.0/openapi.json
-    [[ -z $(git status --porcelain) ]] ||
-    (
-      git status &&
-      git diff &&
-      echo "----------------------------------------------------------------" &&
-      echo "  openapi.json types or actions are out of sync aborting build. " &&
-      echo "----------------------------------------------------------------" &&
-      false
-    )
+SERVER_BASE=${1:-https://cloud.redhat.com}
+
+echo "Comparing current schema against prod API";
+
+yarn schema:integrations:generate -i "${SERVER_BASE}"/api/integrations/v1.0/openapi.json
+yarn schema:notifications:generate -i "${SERVER_BASE}"/api/notifications/v1.0/openapi.json
+
+if [[ -z $(git status src/generated/ --porcelain --untracked-files=no) ]]; then
+  echo "Success."
+else
+  git status src/generated
+  git diff src/generated
+  echo "Error: openapi.json types or actions are out of sync. "
 fi
