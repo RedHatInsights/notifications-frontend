0f89d8e2ab717c3cfa56e2586649c40f882c9645
Josejulio Martínez <jmartine@redhat.com>
2021-09-30 14:06:23 -0500

    Action also needs a min-width

--- src/components/Notifications/EventLog/EventLogActionPopoverContent.tsx
+++ src/components/Notifications/EventLog/EventLogActionPopoverContent.tsx
@@ -1,15 +1,15 @@
 import { Skeleton } from '@patternfly/react-core';
 import { TableComposable, TableVariant, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import { important } from 'csx';
 import * as React from 'react';
+import { style } from 'typestyle';
 
 import Config from '../../../config/Config';
 import { IntegrationType } from '../../../types/Integration';
 import { UUID } from '../../../types/Notification';
-import { style } from 'typestyle';
-import { important } from 'csx';
 
 const actionLabelMap: Record<IntegrationType, string> = Config.integrationNames;
-const statusHeaderClass = style({
+const headerClass = style({
     minWidth: important('90px')
 });
 
@@ -27,9 +27,9 @@ export const EventLogActionPopoverContent: React.FunctionComponent<EventLogActio
         >
             <Thead>
                 <Tr>
-                    <Th>Action</Th>
+                    <Th className={ headerClass }>Action</Th>
                     <Th>Recipient</Th>
-                    <Th className={ statusHeaderClass }>Status</Th>
+                    <Th className={ headerClass }>Status</Th>
                 </Tr>
             </Thead>
             <Tbody>
