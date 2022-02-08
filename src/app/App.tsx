2b637cf954cd5ab96e4203fd00fb33377a0f0346
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-02-08 06:48:08 -0700

    Switch to enable experimental features in stage - This is used to enable the camel types

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,19 @@
 import './App.scss';
 
+import { Switch } from '@patternfly/react-core';
 import { Maintenance, NotAuthorized } from '@redhat-cloud-services/frontend-components';
 import { NotificationsPortal } from '@redhat-cloud-services/frontend-components-notifications';
-import { AppSkeleton, toUtc } from '@redhat-cloud-services/insights-common-typescript';
+import { AppSkeleton, getInsights, InsightsEnvDetector, RenderIfTrue, toUtc } from '@redhat-cloud-services/insights-common-typescript';
 import format from 'date-fns/format';
 import * as React from 'react';
 import { useIntl } from 'react-intl';
 import { useLocation } from 'react-router';
+import { style } from 'typestyle';
 
 import Config from '../config/Config';
 import messages from '../properties/DefinedMessages';
 import { Routes } from '../Routes';
+import { staging } from '../types/Environments';
 import { ServerStatus } from '../types/Server';
 import { getSubApp } from '../utils/Basename';
 import { AppContext } from './AppContext';
@@ -20,10 +23,16 @@ const utcFormat = 'HH:mm';
 const regularFormat = 'hh:mma';
 const timezoneFormat = 'O';
 
+const switchClassname = style({
+    padding: 8
+});
+
 const App: React.ComponentType = () => {
     const intl = useIntl();
     const { rbac, server, isOrgAdmin } = useApp();
     const location = useLocation();
+    const insights = getInsights();
+    const [ usingExperimental, setUsingExperimental ] = React.useState<boolean>(false);
 
     const serviceName = React.useMemo(() => {
         switch (getSubApp(location.pathname)) {
@@ -47,6 +56,17 @@ const App: React.ComponentType = () => {
 
         return false;
     }, [ rbac, location.pathname ]);
+
+    const toggleExperimental = React.useCallback((isEnabled) => {
+        if (isEnabled) {
+            (insights.chrome as any).getEnvironmentOriginal = insights.chrome.getEnvironment;
+            insights.chrome.getEnvironment = () => 'ci';
+        } else {
+            insights.chrome.getEnvironment = ((insights.chrome as any).getEnvironmentOriginal as typeof insights.chrome.getEnvironment);
+        }
+
+        setUsingExperimental(isEnabled);
+    }, [ insights ]);
 
     if (!rbac || !server) {
         return (
@@ -80,6 +100,17 @@ const App: React.ComponentType = () => {
             { hasReadPermissions ? (
                 <>
                     <NotificationsPortal />
+                    <InsightsEnvDetector insights={ insights } onEnvironment={ staging }>
+                        <RenderIfTrue>
+                            <Switch
+                                className={ switchClassname }
+                                isChecked={ usingExperimental }
+                                onChange={ toggleExperimental }
+                                labelOff="Enable experimental features"
+                                label="Disable experimental features"
+                            />
+                        </RenderIfTrue>
+                    </InsightsEnvDetector>
                     <Routes />
                 </>
             ) : (
