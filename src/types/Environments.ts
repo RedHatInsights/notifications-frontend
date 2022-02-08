6a7d439d48be734763df94c78795e8003329f0a8
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-02-08 07:55:41 -0700

    Switch to enable experimental features in stage (#221)
    - This is used to enable the camel types

--- src/types/Environments.ts
+++ src/types/Environments.ts
@@ -18,6 +18,11 @@ export const stagingStableAndAnyProd: Array<Environment> = [
     'prod'
 ];
 
+export const staging: Array<Environment> = [
+    'stage',
+    'stage-beta'
+];
+
 export const isStagingOrProd = (insights: InsightsType) => {
     return stagingAndProd.includes(getInsightsEnvironment(insights));
 };
