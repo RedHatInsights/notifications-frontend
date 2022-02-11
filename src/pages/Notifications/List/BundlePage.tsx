22339ad2ff2e80529d445c60c71d9037e7585f4a
Alexandra Ferdinand <aferdina@redhat.com>
2022-02-11 16:02:03 -0500

    add descriptive text un Notifications, still needs User Pref link.

--- src/pages/Notifications/List/BundlePage.tsx
+++ src/pages/Notifications/List/BundlePage.tsx
@@ -1,4 +1,4 @@
-import { Split, SplitItem } from '@patternfly/react-core';
+import { Split, SplitItem, StackItem } from '@patternfly/react-core';
 import { Main, PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
 import {
     getInsights,
@@ -38,7 +38,11 @@ export const NotificationListBundlePage: React.FunctionComponent<NotificationLis
         <>
             <PageHeader>
                 <Split>
-                    <SplitItem isFilled><PageHeaderTitle { ...pageHeaderTitleProps } /></SplitItem>
+                    <SplitItem isFilled><PageHeaderTitle { ...pageHeaderTitleProps } />
+                        <StackItem>This service allows you to configure which notifications different users within your organization
+                            will be entitled to receiving. To do this, create behavior groups and apply them to different events.
+                            Users will be able to opt-in or out of receiving authorized event notifications in their User Preferences. </StackItem>
+                    </SplitItem>
                     <SplitItem>
                         <InsightsEnvDetector insights={ getInsights() } onEnvironment={ stagingAndProdStable }>
                             <RenderIfFalse>
