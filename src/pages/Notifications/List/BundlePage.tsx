2ba878ab09b3bc744854307afb2ff9e13e7bc7f8
alex <86322239+aferd@users.noreply.github.com>
2022-02-14 15:50:41 -0500

    NOTIF-475 UX Improvements Settings > Notifications (#230)
    * update title on settings/notif to display current bundle
    
    * add descriptive text un Notifications, still needs User Pref link.
    
    * add icon and popover with BG description
    
    * add link to notif desc.
    
    * update link to account for in beta
    
    * update page header title
    
    * update smoketest
    
    * try again

--- src/pages/Notifications/List/BundlePage.tsx
+++ src/pages/Notifications/List/BundlePage.tsx
@@ -1,8 +1,9 @@
-import { Split, SplitItem } from '@patternfly/react-core';
+import { Split, SplitItem, StackItem } from '@patternfly/react-core';
 import { Main, PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
 import {
     getInsights,
     InsightsEnvDetector,
+    localUrl,
     RenderIfFalse
 } from '@redhat-cloud-services/insights-common-typescript';
 import { default as React } from 'react';
@@ -29,7 +30,7 @@ export const NotificationListBundlePage: React.FunctionComponent<NotificationLis
 
     const pageHeaderTitleProps = {
         className: displayInlineClassName,
-        title: Messages.pages.notifications.list.title
+        title: `${Messages.pages.notifications.list.title} | ${props.bundle.displayName}`
     };
 
     const eventLogPageUrl = React.useMemo(() => linkTo.eventLog(props.bundle.name), [ props.bundle.name ]);
@@ -38,7 +39,13 @@ export const NotificationListBundlePage: React.FunctionComponent<NotificationLis
         <>
             <PageHeader>
                 <Split>
-                    <SplitItem isFilled><PageHeaderTitle { ...pageHeaderTitleProps } /></SplitItem>
+                    <SplitItem isFilled><PageHeaderTitle { ...pageHeaderTitleProps } />
+                        <StackItem>This service allows you to configure which notifications different users within your organization
+                            will be entitled to receiving. To do this, create behavior groups and apply them to different events.
+                            Users will be able to opt-in or out of receiving authorized event notifications in their
+                        <a href={ localUrl(`/user-preferences/notifications/${props.bundle.name}`,
+                            getInsights().chrome.isBeta()) }> User Preferences</a>. </StackItem>
+                    </SplitItem>
                     <SplitItem>
                         <InsightsEnvDetector insights={ getInsights() } onEnvironment={ stagingAndProdStable }>
                             <RenderIfFalse>
