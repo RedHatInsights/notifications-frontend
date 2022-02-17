fc32493958b8d67ed5a1873f133a1c13ab259764
Alexandra Ferdinand <aferdina@redhat.com>
2022-02-17 11:30:50 -0500

    fix link routes and revert to hasReadPemissions.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,6 @@ import Config from '../config/Config';
 import messages from '../properties/DefinedMessages';
 import { Routes } from '../Routes';
 import { staging } from '../types/Environments';
-import { Facet } from '../types/Notification';
 import { ServerStatus } from '../types/Server';
 import { getSubApp } from '../utils/Basename';
 import { AppContext } from './AppContext';
@@ -28,21 +27,20 @@ const switchClassname = style({
     padding: 8
 });
 
-interface GetBundleProps {
-    bundles?: Facet;
-}
-
-const App: React.ComponentType<GetBundleProps> = (props) => {
+const App: React.ComponentType = () => {
 
     const intl = useIntl();
     const { rbac, server, isOrgAdmin } = useApp();
     const location = useLocation();
     const insights = getInsights();
     const [ usingExperimental, setUsingExperimental ] = React.useState<boolean>(false);
-    const userPreferences = <a href={ localUrl(`/user-preferences/notifications/${ props.bundles?.name }`,
+
+    const url = window.location.pathname;
+    const filename = url.substring(url.lastIndexOf('/') + 1);
+    const userPreferences = <a href={ localUrl(`/user-preferences/notifications/${ filename }`,
         getInsights().chrome.isBeta()) }> User Preferences </a>;
-    const myUserAccess = <a href={ localUrl(`/settings/my-user-access?bundle=${ props.bundles?.name }`,
-        getInsights().chrome.isBeta()) }> User Preferences </a>;
+    const myUserAccess = <a href={ localUrl(`/settings/my-user-access?bundle=${ filename }`,
+        getInsights().chrome.isBeta()) }> My User Access </a>;
 
     const serviceName = React.useMemo(() => {
         switch (getSubApp(location.pathname)) {
@@ -55,13 +53,13 @@ const App: React.ComponentType<GetBundleProps> = (props) => {
         }
     }, [ intl, location.pathname ]);
 
-    const hasWritePermissions = React.useMemo(() => {
+    const hasReadPermissions = React.useMemo(() => {
         const appId = getSubApp(location.pathname);
         switch (appId) {
             case Config.integrations.subAppId:
-                return rbac?.canWriteIntegrationsEndpoints;
+                return rbac?.canReadIntegrationsEndpoints;
             case Config.notifications.subAppId:
-                return rbac?.canWriteNotifications;
+                return rbac?.canReadNotifications;
         }
 
         return false;
@@ -107,7 +105,7 @@ const App: React.ComponentType<GetBundleProps> = (props) => {
             server,
             isOrgAdmin: !!isOrgAdmin
         } }>
-            { hasWritePermissions ? (
+            { hasReadPermissions ? (
                 <>
                     <NotificationsPortal />
                     <InsightsEnvDetector insights={ insights } onEnvironment={ staging }>
@@ -126,9 +124,9 @@ const App: React.ComponentType<GetBundleProps> = (props) => {
             ) : (
                 <>
                     <NotAuthorized
-                        description={ `Contact your organization administrator for more information or visit
-                        ${ myUserAccess } to learn more about your permissions. To manage your notifications,
-                        go to your ${ userPreferences }.` }
+                        description={ <> Contact your organization administrator for more information or visit
+                            { myUserAccess } to learn more about your permissions. To manage your notifications,
+                        go to your { userPreferences }. </> }
                         serviceName={ serviceName }
                     />
                 </>
