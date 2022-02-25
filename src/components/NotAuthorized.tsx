71ddb46bd7bfb50fd81892ecc09589a35d74bd3d
Alexandra Ferdinand <aferdina@redhat.com>
2022-02-25 10:47:55 -0500

    fix title to show displayName

--- src/components/NotAuthorized.tsx
+++ src/components/NotAuthorized.tsx
@@ -2,12 +2,14 @@ import { Split, SplitItem, StackItem } from '@patternfly/react-core';
 import { Main, PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
 import { NotAuthorized } from '@redhat-cloud-services/frontend-components';
 import { getInsights, localUrl } from '@redhat-cloud-services/insights-common-typescript';
-import { default as React } from 'react';
+import { default as React, useMemo } from 'react';
 import { useIntl } from 'react-intl';
 import { useLocation, useParams } from 'react-router-dom';
 
 import Config from '../config/Config';
 import messages from '../properties/DefinedMessages';
+import { useGetBundles } from '../services/Notifications/GetBundles';
+import { Facet } from '../types/Notification';
 import { getSubApp } from '../utils/Basename';
 
 interface NotificationListPageParams {
@@ -17,6 +19,13 @@ interface NotificationListPageParams {
 export const NotAuthorizedPage: React.FunctionComponent = () => {
 
     const { bundleName } = useParams<NotificationListPageParams>();
+    const getBundles = useGetBundles();
+    const bundles: Facet | undefined = useMemo (() => {
+        if (getBundles.payload?.status === 200) {
+            return getBundles.payload.value.find(b => b.name === bundleName);
+        }
+    }, [ bundleName, getBundles.payload?.status, getBundles.payload?.value ]);
+
     const location = useLocation();
     const intl = useIntl();
 
@@ -42,11 +51,11 @@ export const NotAuthorizedPage: React.FunctionComponent = () => {
 
     const title = React.useMemo(() => {
         if (serviceName === 'Notifications') {
-            return `${ serviceName } | ${ bundleName }`;
+            return `${ serviceName } | ${ bundles?.displayName }`;
         } else {
             return `${ serviceName }`;
         }
-    }, [ bundleName, serviceName ]);
+    }, [ bundles?.displayName, serviceName ]);
 
     const description = React.useMemo(() => {
         if (serviceName === 'Notifications') {
