10380ff95c1363966c4f15cc55742227238f7325
Yasin-Darar <80718590+Yasin-Darar@users.noreply.github.com>
2022-03-21 10:39:31 -0400

    NOTIF-397 Disable repeated Integration actions in Behavior Groups (#253)
    * NOTIF-397: Implemented fix to disable select box options for integrations | TODO: test
    
    * NOTIF-397: completed tests | ran linter | updated with comments from josejulio
    
    * NOTIF-397: added description to indicate why existing integrations are disabled

--- src/components/Notifications/Form/useRecipientOptionMemo.tsx
+++ src/components/Notifications/Form/useRecipientOptionMemo.tsx
@@ -2,20 +2,27 @@ import { SelectOption } from '@patternfly/react-core';
 import assertNever from 'assert-never';
 import * as React from 'react';
 
-import { NotificationRecipient, Recipient } from '../../../types/Recipient';
+import { IntegrationRecipient, NotificationRecipient, Recipient } from '../../../types/Recipient';
 import { RecipientOption } from './RecipientOption';
 import { ReducerState } from './useTypeaheadReducer';
 
-const mapper = (r: Recipient) => {
-    let description;
+const mapper = (r: Recipient, existingIntegrations?: Set<string>) => {
+    let isDisabled = false;
+    let description: string | undefined = undefined;
+
     if (r instanceof NotificationRecipient) {
         description = r.description;
     }
 
-    return <SelectOption key={ r.getKey() } value={ new RecipientOption(r) } description={ description } />;
+    else if (r instanceof IntegrationRecipient) {
+        isDisabled = !!existingIntegrations?.has(r.integration.id);
+        description = isDisabled ? 'This integration has already been added' : description;
+    }
+
+    return <SelectOption key={ r.getKey() } value={ new RecipientOption(r) } description={ description } isDisabled={ isDisabled } />;
 };
 
-export const useRecipientOptionMemo = (state: ReducerState<Recipient>) => {
+export const useRecipientOptionMemo = (state: ReducerState<Recipient>, existingIntegrations?: Set<string>) => {
     return React.useMemo(() => {
         if (state.show === 'default') {
             if (state.loadingDefault) {
@@ -25,7 +32,7 @@ export const useRecipientOptionMemo = (state: ReducerState<Recipient>) => {
                     value="Loading..."
                 /> ];
             } else {
-                return state.defaultValues.map(mapper);
+                return state.defaultValues.map(recipient => mapper(recipient, existingIntegrations));
             }
         } else if (state.show === 'filter') {
             if (state.loadingFilter) {
@@ -35,10 +42,10 @@ export const useRecipientOptionMemo = (state: ReducerState<Recipient>) => {
                     value="Loading..."
                 /> ];
             } else {
-                return state.filterValues.map(mapper);
+                return state.filterValues.map(recipient => mapper(recipient, existingIntegrations));
             }
         }
 
         assertNever(state.show);
-    }, [ state ]);
+    }, [ state, existingIntegrations ]);
 };
