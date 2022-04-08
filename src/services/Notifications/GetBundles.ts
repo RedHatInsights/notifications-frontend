66a878a32438b6570a62e7d03ecd587187601cb8
Viliam Krizan <vkrizan@redhat.com>
2022-04-08 14:40:19 +0200

    feat(services): create useGetBundleByName

--- src/services/Notifications/GetBundles.ts
+++ src/services/Notifications/GetBundles.ts
@@ -1,7 +1,26 @@
 import { useQuery } from 'react-fetching-library';
 
-import { Operations } from '../../generated/OpenapiNotifications';
+import { Operations, Schemas } from '../../generated/OpenapiNotifications';
 
 const getBundlesAction = (includeApplications: boolean) => Operations.NotificationServiceGetBundleFacets.actionCreator(includeApplications);
 
-export const useGetBundles = (includeApplications?: boolean) => useQuery(getBundlesAction(!!includeApplications));
+export const useGetBundles = (includeApplications?: boolean, initFetch = true) =>
+    useQuery(getBundlesAction(!!includeApplications), initFetch);
+
+export const useGetBundleByName = () => {
+    const { query } = useGetBundles(false, false);  // includeApplications = false, initFetch = false
+    return async (bundleName: string)  => {
+        const response = await query();
+        const payload = response.payload as Operations.NotificationServiceGetApplicationsFacets.Payload;
+        if (response.errorObject) {
+            throw response.errorObject;
+        }
+
+        if (response.error || !payload) {
+            throw new Error(`Unable to retrieve bundles, status ${payload.status}`);
+        }
+
+        const value = payload.value as Schemas.Bundle[];
+        return value.find(bundle => bundle.name === bundleName);
+    };
+};
