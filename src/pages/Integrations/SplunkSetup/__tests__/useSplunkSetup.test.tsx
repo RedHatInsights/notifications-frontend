04adeef4f87a64d6e9db8dbedc069453f28d40d9
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-06-24 10:35:27 -0600

    Shows an error message if we are using an unique name  - Splunk integration:     Try to append "(%d)" and try again until succeed

---
+++ src/pages/Integrations/SplunkSetup/__tests__/useSplunkSetup.test.tsx
@@ -0,0 +1,75 @@
+import { useSaveBehaviorGroupMutation } from '../../../../services/Notifications/SaveBehaviorGroup';
+import { useGetBundleByName } from '../../../../services/Notifications/GetBundles';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useCreateSplunkBehaviorGroup } from '../useSplunkSetup';
+
+jest.mock('../../../../services/Notifications/SaveBehaviorGroup', () => {
+    const actual = jest.requireActual('../../../../services/Notifications/SaveBehaviorGroup');
+
+    return {
+        ...actual,
+        useSaveBehaviorGroupMutation: jest.fn()
+    };
+});
+
+jest.mock('../../../../services/Notifications/GetBundles', () => ({
+    useGetBundleByName: jest.fn()
+}));
+
+describe('src/pages/Integrations/SplunkSetup/useSplunkSetup', () => {
+    describe('useCreateSplunkBehaviorGroup', () => {
+        it('should retry if the name already exists', async () => {
+            const existingBehaviorGroups = [
+                'foo',
+                'SPLUNK_AUTOMATION_GROUP',
+                'SPLUNK_AUTOMATION_GROUP (1)',
+                'SPLUNK_AUTOMATION_GROUP (2)',
+                'SPLUNK_AUTOMATION_GROUP (3)',
+                'SPLUNK_AUTOMATION_GROUP (5)'
+            ];
+
+            (useGetBundleByName as jest.Mock).mockImplementation(() => () => ({
+                id: '1'
+            }));
+
+            (useSaveBehaviorGroupMutation as jest.Mock).mockImplementation(() => ({
+                mutate: (behaviorGroup) => {
+                    if (existingBehaviorGroups.includes(behaviorGroup.displayName)) {
+                        return {
+                            error: true,
+                            payload: {
+                                value: 'already exists'
+                            }
+                        };
+                    }
+
+                    return {
+                        error: false,
+                        payload: {
+                            value: behaviorGroup
+                        }
+                    };
+                }
+            }));
+            const { result } = renderHook(() => useCreateSplunkBehaviorGroup());
+            let value;
+
+            await act(async () => {
+                value = await result.current({
+                    behaviorGroupName: 'SPLUNK_AUTOMATION_GROUP',
+                    bundleName: 'bundle'
+                });
+            });
+            expect(value.displayName).toBe('SPLUNK_AUTOMATION_GROUP (4)');
+            existingBehaviorGroups.push(value.displayName);
+
+            await act(async () => {
+                value = await result.current({
+                    behaviorGroupName: 'SPLUNK_AUTOMATION_GROUP',
+                    bundleName: 'bundle'
+                });
+            });
+            expect(value.displayName).toBe('SPLUNK_AUTOMATION_GROUP (6)');
+        });
+    });
+});
