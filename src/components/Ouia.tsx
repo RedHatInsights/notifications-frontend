b124a097eb6d6260c002cbe1250964cb15d99c94
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-07-04 06:40:51 -0600

    NOTIF-705 Remove unused dependencies

--- src/components/Ouia.tsx
+++ src/components/Ouia.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 import { getOuiaProps } from '../utils/getOuiaProps';
 
-export const ouia = <P extends any>(Component: React.FunctionComponent<P>, type: string): React.FunctionComponent<OuiaComponentProps & P> => {
+export const ouia = <P, >(Component: React.FunctionComponent<P>, type: string): React.FunctionComponent<OuiaComponentProps & P> => {
 
     const Wrapped: React.FunctionComponent<OuiaComponentProps & P> = props => {
         return (
