1a30d31f99618609e90ad00ef35c13d714193aaa
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-08-10 07:05:45 -0600

    NOTIF-747 Modifies Behavior group list scroll direction (horizontally now)

--- src/components/Notifications/BehaviorGroup/__tests__/BehaviorGroupCardList.test.tsx
+++ src/components/Notifications/BehaviorGroup/__tests__/BehaviorGroupCardList.test.tsx
@@ -1,7 +1,6 @@
-import { act, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import * as React from 'react';
 
-import { getLastObserver } from '../../../../../config/testutils/ResizeObserverMock';
 import { waitForAsyncEvents } from '../../../../../test/TestUtils';
 import { BehaviorGroup, NotificationType } from '../../../../types/Notification';
 import { BehaviorGroupCardList } from '../BehaviorGroupCardList';
@@ -15,19 +14,6 @@ const Container: React.FunctionComponent<{
         </div>
     </div>
 );
-
-const makeRect = (contentRect?: Partial<DOMRectReadOnly>) => ({
-    bottom: 0,
-    height: 0,
-    left: 0,
-    right: 0,
-    top: 0,
-    width: 0,
-    x: 0,
-    y: 0,
-    toJSON: () => 'foo',
-    ...contentRect
-});
 
 describe('src/components/Notifications/BehaviorGroup/BehaviorGroupCardList', () => {
     it('Behavior groups created inside a display:none section are still show correctly after when setting display:block', async () => {
@@ -47,8 +33,6 @@ describe('src/components/Notifications/BehaviorGroup/BehaviorGroupCardList', () 
             isDefault: false
         }];
 
-        window. HTMLDivElement.prototype.getBoundingClientRect = () => makeRect();
-
         const { rerender } = render(
             <Container hideContent={ true }>
                 <BehaviorGroupCardList onEdit={ jest.fn() } onDelete={ jest.fn() } behaviorGroups={ bg } />
@@ -57,11 +41,6 @@ describe('src/components/Notifications/BehaviorGroup/BehaviorGroupCardList', () 
 
         expect(screen.getByText(/my group/i)).toBeInTheDocument();
         expect(screen.getByText(/my group/i)).not.toBeVisible();
-        expect(screen.getByTestId('card-list-container')).toHaveStyle('max-height:0px');
-
-        window. HTMLDivElement.prototype.getBoundingClientRect = () => makeRect({
-            height: 100
-        });
 
         rerender(
             <Container hideContent={ false }>
@@ -69,16 +48,9 @@ describe('src/components/Notifications/BehaviorGroup/BehaviorGroupCardList', () 
             </Container>
         );
 
-        // Simulate resize
-        act(() => {
-            getLastObserver().mockEvent({
-                height: 100
-            });
-        });
         await waitForAsyncEvents();
 
         expect(screen.getByText(/my group/i)).toBeInTheDocument();
         expect(screen.getByText(/my group/i)).toBeVisible();
-        expect(screen.getByTestId('card-list-container')).toHaveStyle('max-height:100px');
     });
 });
