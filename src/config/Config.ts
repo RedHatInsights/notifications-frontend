e73e69289020adbf4f2d67235df7afb4f54be154
Gwenneg Lepage <glepage@redhat.com>
2023-03-24 14:21:04 +0100

    RHCLOUD-24868 Fix Teams integration type name

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -37,7 +37,7 @@ const integrationTypes: Record<IntegrationType, IntegrationTypeConfigBase> = {
         name: 'Email'
     },
     [IntegrationType.TEAMS]: {
-        name: 'Teams'
+        name: 'Microsoft Teams'
     }
 };
 
