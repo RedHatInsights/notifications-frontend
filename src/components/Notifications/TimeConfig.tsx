c1efa76daa48095966ea6bc7d30bf8a343cc1d43
Alexandra Ferdinand <aferdina@redhat.com>
2023-04-19 11:20:15 -0400

    begin moving things vertical

--- src/components/Notifications/TimeConfig.tsx
+++ src/components/Notifications/TimeConfig.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Dropdown, DropdownItem, DropdownToggle, HelperText, HelperTextItem, PageSection, Radio, Split, SplitItem, Stack, StackItem,
+import { Card, CardBody, Dropdown, DropdownItem, DropdownToggle, HelperText, HelperTextItem, PageSection, Radio, Stack, StackItem,
     Text, TextVariants, TimePicker, Title } from '@patternfly/react-core';
 import React from 'react';
 import timezones from 'timezones.json';
@@ -45,65 +45,57 @@ export const TimeConfigComponent: React.FunctionComponent = () => {
             <PageSection>
                 <Card>
                     <CardBody>
-                        <Split>
-                            <SplitItem isFilled>
-                                <Stack hasGutter>
-                                    <StackItem>
-                                        <Title headingLevel='h2'>Action Settings</Title>
-                                    </StackItem>
-                                    <StackItem>
-                                        <Text component={ TextVariants.p }>Daily digest email reciept</Text>
-                                        <HelperText>
-                                            <HelperTextItem variant="indeterminate">
+                        <Stack hasGutter>
+                            <StackItem>
+                                <Title headingLevel='h2'>Action Settings</Title>
+                            </StackItem>
+                            <StackItem>
+                                <Text component={ TextVariants.p }>Daily digest email reciept</Text>
+                                <HelperText>
+                                    <HelperTextItem variant="indeterminate">
                                                 Schedule the time at which to send your accounts daily digest email
-                                            </HelperTextItem>
-                                        </HelperText>
-                                    </StackItem>
-                                </Stack>
-                            </SplitItem>
-                            <SplitItem isFilled>
-                                <Stack hasGutter>
-                                    <StackItem >
-                                        <Radio
-                                            isChecked={ radioSelect && !showCustomSelect }
-                                            onChange={ handleRadioSelect }
-                                            id='settings-time-config'
-                                            label='Default time'
-                                            description='12:00 UTC'
-                                            name='radio-select'>
-                                        </Radio>
-                                    </StackItem>
-                                    <StackItem>
-                                        <Radio
-                                            isChecked={ radioSelect && showCustomSelect }
-                                            onChange={ handleCustomRadioSelect }
-                                            id='settings-time-config'
-                                            label='Custom time'
-                                            description='Choose specific time and time zone'
-                                            name='radio-select'>
-                                        </Radio>
-                                    </StackItem>
-                                    {showCustomSelect && (
-                                        <><StackItem>
-                                            <Text component={ TextVariants.h6 }>Time</Text>
-                                            <TimePicker onChange={ handleTimeSelect }
-                                                width='263px' stepMinutes={ 15 } placeholder='12:00' is24Hour />
-                                        </StackItem><StackItem>
-                                            <Text component={ TextVariants.h6 }>Time zone</Text>
-                                            <Dropdown
-                                                className={ dropDownClassName }
-                                                toggle={ <DropdownToggle isOpen={ isOpen } id="timezone" onToggle={ () => setIsOpen(!isOpen) }>
+                                    </HelperTextItem>
+                                </HelperText>
+                            </StackItem>
+                            <StackItem >
+                                <Radio
+                                    isChecked={ radioSelect && !showCustomSelect }
+                                    onChange={ handleRadioSelect }
+                                    id='settings-time-config'
+                                    label='Default time'
+                                    description='00:00 UTC'
+                                    name='radio-select'>
+                                </Radio>
+                            </StackItem>
+                            <StackItem>
+                                <Radio
+                                    isChecked={ radioSelect && showCustomSelect }
+                                    onChange={ handleCustomRadioSelect }
+                                    id='settings-time-config'
+                                    label='Custom time'
+                                    description='Choose specific time and time zone'
+                                    name='radio-select'>
+                                </Radio>
+                            </StackItem>
+                            {showCustomSelect && (
+                                <><StackItem>
+                                    <Text component={ TextVariants.h6 }>Time</Text>
+                                    <TimePicker onChange={ handleTimeSelect }
+                                        width='263px' stepMinutes={ 15 } placeholder='12:00' is24Hour />
+                                </StackItem><StackItem>
+                                    <Text component={ TextVariants.h6 }>Time zone</Text>
+                                    <Dropdown
+                                        className={ dropDownClassName }
+                                        toggle={ <DropdownToggle isOpen={ isOpen } id="timezone" onToggle={ () => setIsOpen(!isOpen) }>
                                                     (UTC-00:00) Universal Time
-                                                </DropdownToggle> }
-                                                isOpen={ isOpen }
-                                                onSelect={ handleTimeSelect }
-                                                menuAppendTo={ () => document.body }
-                                                dropdownItems={ dropdownItems }>
-                                            </Dropdown>
-                                        </StackItem></>)}
-                                </Stack>
-                            </SplitItem>
-                        </Split>
+                                        </DropdownToggle> }
+                                        isOpen={ isOpen }
+                                        onSelect={ handleTimeSelect }
+                                        menuAppendTo={ () => document.body }
+                                        dropdownItems={ dropdownItems }>
+                                    </Dropdown>
+                                </StackItem></>)}
+                        </Stack>
                     </CardBody>
                 </Card>
             </PageSection>
