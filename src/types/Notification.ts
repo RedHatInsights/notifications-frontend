37f4fcab50fc5ca549fa1f94ced87c26df713b0f
alex <86322239+aferd@users.noreply.github.com>
2023-07-21 10:32:59 -0400

    UX updates to timezone config (#428)
    * UX updates to timezone config
    
    * trying to fix save func
    
    * remove comment
    
    * current state
    
    * latest
    
    * Use time from input and send it to backend
    
    * add loading indicator, set time when default is chosen
    
    * remove prop
    
    * change response status
    
    Co-authored-by: Josejulio Martínez <josejulio@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Josejulio Martínez <josejulio@users.noreply.github.com>

--- src/types/Notification.ts
+++ src/types/Notification.ts
@@ -55,6 +55,8 @@ export enum NotificationType {
 
 export type Facet = Schemas.Facet;
 
+export type LocalTime = Schemas.LocalTime
+
 export type ServerNotificationRequest = Schemas.EventType;
 export type ServerNotificationResponse = Schemas.EventType;
 
@@ -128,3 +130,4 @@ export const areActionsEqual = (actions1: ReadonlyArray<Action | undefined>, act
     // Order matters here, no sorting.
     return integrations1.every((val, index) => val === integrations2[index]);
 };
+
