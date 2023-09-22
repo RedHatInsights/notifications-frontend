0d30d5afa4aed1c8a97c0f4f5b8dc38420eed3dc
Filip Hlavac <filip.hlavac01@gmail.com>
2023-09-22 12:13:46 +0200

    Wrap integrations app with Router

--- src/IntegrationsEntry.tsx
+++ src/IntegrationsEntry.tsx
@@ -8,11 +8,13 @@ import { validateSchemaResponseInterceptor } from 'openapi2typescript/react-fetc
 import React from 'react';
 import { ClientContextProvider } from 'react-fetching-library';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import messages from '../locales/data.json';
 import IntegrationsApp from './app/IntegrationsApp';
 import { AppEntryProps } from './AppEntry';
 import { getNotificationsRegistry } from './store/Store';
+import { getBaseName } from './utils/Basename';
 
 enableMapSet();
 
@@ -30,9 +32,11 @@ const IntegrationsEntry: React.FunctionComponent<AppEntryProps> = (props) => {
     return (
         <IntlProvider locale={ navigator.language.slice(0, 2) } messages={ messages } onError={ console.log }>
             <Provider store={ store }>
-                <ClientContextProvider client={ client }>
-                    <IntegrationsApp />
-                </ClientContextProvider>
+                <Router basename={ getBaseName(window.location.pathname) }>
+                    <ClientContextProvider client={ client }>
+                        <IntegrationsApp />
+                    </ClientContextProvider>
+                </Router>
             </Provider>
         </IntlProvider>
     );
