846f47756e836718ee972a9da7f66283a5f5b187
Zein Sleiman <zsleiman@redhat.com>
2023-11-02 10:23:44 -0500

    lint

--- src/components/Notifications/ConnectedTimeConfig.tsx
+++ src/components/Notifications/ConnectedTimeConfig.tsx
@@ -1,22 +1,21 @@
+import React from 'react';
 import { Provider } from 'react-redux';
-import { TimeConfigComponent } from './TimeConfig';
-import React, { useEffect, useMemo, useState } from 'react';
+
 import { AppEntryProps } from '../../AppEntry';
 import { getNotificationsRegistry } from '../../store/Store';
-
-
+import { TimeConfigComponent } from './TimeConfig';
 
 export const ConnectedTimeConfig: React.FunctionComponent<AppEntryProps> = (props) => {
 
-  const store = React.useMemo(() => {
-    const registry = props.logger ? getNotificationsRegistry(props.logger) : getNotificationsRegistry();
-    return registry.getStore();
-  }, [ props.logger ]);
+    const store = React.useMemo(() => {
+        const registry = props.logger ? getNotificationsRegistry(props.logger) : getNotificationsRegistry();
+        return registry.getStore();
+    }, [ props.logger ]);
 
-  return (
-    <Provider store={ store }>
-      <TimeConfigComponent/>
-    </Provider> 
-  )
-  
-}
+    return (
+        <Provider store={ store }>
+            <TimeConfigComponent />
+        </Provider>
+    );
+
+};
