5b55747bf86daf5cdf8ca48a402243c37a96bcb6
Bryan Florkiewicz <bflorkie@redhat.com>
2023-11-09 20:52:55 -0500

    Fix smoketest and pin versions

--- test/VerboseErrorBoundary.tsx
+++ test/VerboseErrorBoundary.tsx
@@ -1,29 +1,29 @@
 import * as React from 'react';
 
 interface VerboseErrorState {
-    hasError: boolean;
-    error?: any;
+  hasError: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error?: any;
 }
 
-export class VerboseErrorBoundary extends React.Component<unknown, VerboseErrorState> {
+export class VerboseErrorBoundary extends React.Component<
+  unknown,
+  VerboseErrorState
+> {
+  constructor(props: Readonly<unknown>) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-    constructor(props: Readonly<unknown>) {
-        super(props);
-        this.state = { hasError: false };
+  static getDerivedStateFromError(error): VerboseErrorState {
+    return { hasError: true, error };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div> {this.state.error.toString()} </div>;
     }
 
-    static getDerivedStateFromError(error): VerboseErrorState {
-        return { hasError: true, error };
-    }
-
-    render() {
-        if (this.state.hasError) {
-            return (
-                <div> { this.state.error.toString() } </div>
-            );
-        }
-
-        return this.props.children;
-    }
-
+    return this.props.children;
+  }
 }
