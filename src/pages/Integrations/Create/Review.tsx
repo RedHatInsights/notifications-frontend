ba03b012d202ccc1de8010a961026c4a42fa88ab
Karel Hala <khala@redhat.com>
2023-11-15 11:09:54 +0100

    Generic review step calculaed from values and fields

--- src/pages/Integrations/Create/Review.tsx
+++ src/pages/Integrations/Create/Review.tsx
@@ -7,11 +7,34 @@ import {
   Text,
   TextVariants,
 } from '@patternfly/react-core';
-import React from 'react';
+import React, { Fragment } from 'react';
+
+const getFields = (fields) =>
+  fields.flatMap(({ fields, ...rest }) => {
+    if (fields) {
+      return getFields(fields);
+    }
+    return { ...rest };
+  });
+
+const mapFieldValues = (values, fields) => {
+  const allFields = getFields(fields);
+  return Object.entries(values)
+    .filter(([, value]) => !!value)
+    .map(([key, value]) => {
+      const currField = allFields.find(({ name }) => name === key);
+      return {
+        ...currField,
+        value,
+      };
+    })
+    .filter(({ value }) => !!value);
+};
 
 const Review: React.FunctionComponent = () => {
   const formOptions = useFormApi();
-  const { 'integration-name': name } = formOptions.getState().values;
+  const values = formOptions.getState().values;
+  const labelsWithValues = mapFieldValues(values, formOptions.schema.fields);
 
   return (
     <Stack hasGutter>
@@ -19,12 +42,16 @@ const Review: React.FunctionComponent = () => {
         <Stack hasGutter>
           <StackItem>
             <Grid>
-              <GridItem md={3}>
-                <Text component={TextVariants.h4}>Integration name</Text>
-              </GridItem>
-              <GridItem md={9}>
-                <Text component={TextVariants.p}>{name}</Text>
-              </GridItem>
+              {labelsWithValues.map((field) => (
+                <Fragment key={field.name}>
+                  <GridItem md={3}>
+                    <Text component={TextVariants.h4}>{field.label}</Text>
+                  </GridItem>
+                  <GridItem md={9}>
+                    <Text component={TextVariants.p}>{field.value}</Text>
+                  </GridItem>
+                </Fragment>
+              ))}
             </Grid>
           </StackItem>
         </Stack>
