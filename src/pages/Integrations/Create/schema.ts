670d3c3f217d6d2585355827295309ed44d35dfa
radekkaluzik <51480040+radekkaluzik@users.noreply.github.com>
2023-11-16 16:22:13 +0100

    Add integration - webhooks (#496)

--- src/pages/Integrations/Create/schema.ts
+++ src/pages/Integrations/Create/schema.ts
@@ -4,6 +4,7 @@ import {
 } from '@data-driven-forms/react-form-renderer';
 import { integrationsStep } from './IntegrationsStep';
 import { SUMMARY } from './CreateWizard';
+import { IntegrationCategory } from '../../../types/Integration';
 export const schema = (category) => ({
   fields: [
     {
@@ -15,24 +16,87 @@ export const schema = (category) => ({
         'Configure integrations between third-party tools and the Red Hat Hybrid Cloud Console.',
       name: 'add-integration-wizard',
       fields: [
-        integrationsStep(category),
+        ...([IntegrationCategory.COMMUNICATIONS].includes(category)
+          ? [integrationsStep(category)]
+          : []),
         {
           title: 'Enter details',
           name: 'details',
           nextStep: 'review',
           fields: [
-            {
-              component: componentTypes.TEXT_FIELD,
-              name: 'integration-name',
-              type: 'text',
-              label: 'Integration name',
-              isRequired: true,
-              validate: [
-                {
-                  type: validatorTypes.REQUIRED,
-                },
-              ],
-            },
+            ...([IntegrationCategory.WEBHOOKS].includes(category)
+              ? [
+                  {
+                    component: componentTypes.PLAIN_TEXT,
+                    name: 'integration-details-title',
+                    label: 'Enter integration details',
+                    variant: 'h3',
+                  },
+                  {
+                    component: componentTypes.PLAIN_TEXT,
+                    name: 'integration-details-subtitle',
+                    label: 'Enter the details for your integration.',
+                    variant: 'p',
+                  },
+                  {
+                    component: componentTypes.TEXT_FIELD,
+                    name: 'integration-name',
+                    type: 'text',
+                    label: 'Integration name',
+                    isRequired: true,
+                    validate: [
+                      {
+                        type: 'required',
+                      },
+                    ],
+                  },
+                  {
+                    component: componentTypes.TEXT_FIELD,
+                    name: 'endpoint-url',
+                    type: 'text',
+                    label: 'Endpoint URL',
+                    helperText: 'URL must include "http://"',
+                    isRequired: true,
+                    validate: [
+                      {
+                        type: 'required',
+                      },
+                      {
+                        type: validatorTypes.URL,
+                        message: 'URL must include "http://"',
+                      },
+                    ],
+                  },
+                  {
+                    component: componentTypes.TEXT_FIELD,
+                    name: 'secret-token',
+                    type: 'text',
+                    label: 'Secret token',
+                    helperText:
+                      'The defined secret token is sent as a "X-Insight-Token" header on the request.',
+                    isRequired: false,
+                  },
+                  {
+                    name: 'inline-info-alert',
+                    component: 'inline-alert',
+                    title: 'SSL verification will be enabled',
+                    variant: 'info',
+                  },
+                ]
+              : [
+                  {
+                    component: componentTypes.TEXT_FIELD,
+                    name: 'integration-name',
+                    type: 'text',
+                    label: 'Integration name',
+                    isRequired: true,
+                    validate: [
+                      {
+                        type: validatorTypes.REQUIRED,
+                      },
+                    ],
+                  },
+                ]),
           ],
         },
         {
@@ -40,8 +104,8 @@ export const schema = (category) => ({
           name: 'review',
           fields: [
             {
-              component: [SUMMARY],
-              name: [SUMMARY],
+              component: SUMMARY,
+              name: SUMMARY,
             },
           ],
         },
