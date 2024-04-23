47eb7c6af60377c6e7dc06f0a0fe0d31c5079720
Karel Hala <khala@redhat.com>
2024-04-23 14:43:47 +0200

    Fix broken typeahead

--- src/components/Notifications/Form/IntegrationRecipientTypeahead.tsx
+++ src/components/Notifications/Form/IntegrationRecipientTypeahead.tsx
@@ -1,13 +1,6 @@
-import {
-  Select,
-  SelectOption,
-  SelectOptionObject,
-  SelectVariant,
-} from '@patternfly/react-core/deprecated';
 import { OuiaComponentProps } from '@redhat-cloud-services/insights-common-typescript';
 import { useFormikContext } from 'formik';
 import * as React from 'react';
-import { usePrevious } from 'react-use';
 import { DeepPartial } from 'ts-essentials';
 
 import Config from '../../../config/Config';
@@ -19,11 +12,23 @@ import {
   NotificationType,
 } from '../../../types/Notification';
 import { IntegrationRecipient } from '../../../types/Recipient';
-import { getOuiaProps } from '../../../utils/getOuiaProps';
 import { useRecipientContext } from '../RecipientContext';
 import { RecipientOption } from './RecipientOption';
-import { useRecipientOptionMemo } from './useRecipientOptionMemo';
 import { useTypeaheadReducer } from './useTypeaheadReducer';
+import {
+  MenuToggle,
+  MenuToggleElement,
+  Select,
+  SelectList,
+  SelectOption,
+  Skeleton,
+  TextInputGroup,
+  TextInputGroupMain,
+} from '@patternfly/react-core';
+
+function isRecipientOption(option: unknown): option is RecipientOption {
+  return (option as RecipientOption).recipient !== undefined;
+}
 
 export interface IntegrationRecipientTypeaheadProps extends OuiaComponentProps {
   selected: Partial<IntegrationRef> | undefined;
@@ -36,53 +41,24 @@ export interface IntegrationRecipientTypeaheadProps extends OuiaComponentProps {
 
 export const IntegrationRecipientTypeahead: React.FunctionComponent<
   IntegrationRecipientTypeaheadProps
-> = (props) => {
-  const [isOpen, setOpen] = React.useState(false);
-  const prevOpen = usePrevious(isOpen);
+> = ({ integrationType, selected, onSelected }) => {
+  const chooseText = React.useMemo(
+    () =>
+      `Choose ${Config.integrations.types[integrationType].name.toLowerCase()}`,
+    [integrationType]
+  );
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [inputValue, setInputValue] = React.useState<string>('');
+  const [focusedItemIndex, setFocusedItemIndex] = React.useState<number | null>(
+    null
+  );
+  const [activeItem, setActiveItem] = React.useState<string | null>(null);
+  const textInputRef = React.useRef<HTMLInputElement>();
 
   const { getIntegrations } = useRecipientContext();
   const { values } = useFormikContext<DeepPartial<BehaviorGroup>>();
 
   const [state, dispatchers] = useTypeaheadReducer<IntegrationRecipient>();
-
-  const toggle = React.useCallback(
-    (isOpen: boolean) => {
-      setOpen(isOpen);
-    },
-    [setOpen]
-  );
-
-  React.useEffect(() => {
-    const onOpenChange = props.onOpenChange;
-    if (prevOpen !== undefined && prevOpen !== isOpen) {
-      onOpenChange && onOpenChange(isOpen);
-    }
-  }, [prevOpen, isOpen, props.onOpenChange]);
-
-  React.useEffect(() => {
-    getIntegrations(props.integrationType, '').then((integrations) => {
-      const defaults = integrations.map((i) => new IntegrationRecipient(i));
-      dispatchers.setDefaults(defaults);
-    });
-  }, [getIntegrations, props.integrationType, dispatchers]);
-
-  React.useEffect(() => {
-    if (state.loadingFilter) {
-      getIntegrations(props.integrationType, state.lastSearch).then(
-        (integrations) =>
-          dispatchers.setFilterValue(
-            state.lastSearch,
-            integrations.map((i) => new IntegrationRecipient(i))
-          )
-      );
-    }
-  }, [
-    getIntegrations,
-    props.integrationType,
-    state.loadingFilter,
-    state.lastSearch,
-    dispatchers,
-  ]);
 
   const existingIntegrations = React.useMemo(() => {
     const integrationActions = (values.actions ?? [])
@@ -92,49 +68,80 @@ export const IntegrationRecipientTypeahead: React.FunctionComponent<
     return new Set<string>(integrationActions);
   }, [values]);
 
+  React.useEffect(() => {
+    setInputValue(selected?.name as string);
+  }, [selected]);
+
+  React.useEffect(() => {
+    getIntegrations(integrationType, '').then((integrations) => {
+      const defaults = integrations.map((i) => new IntegrationRecipient(i));
+      dispatchers.setDefaults(defaults);
+    });
+  }, [getIntegrations, integrationType, dispatchers]);
+
+  React.useEffect(() => {
+    if (state.loadingFilter) {
+      getIntegrations(integrationType, state.lastSearch).then((integrations) =>
+        dispatchers.setFilterValue(
+          state.lastSearch,
+          integrations.map((i) => new IntegrationRecipient(i))
+        )
+      );
+    }
+  }, [
+    getIntegrations,
+    integrationType,
+    state.loadingFilter,
+    state.lastSearch,
+    dispatchers,
+  ]);
+
   const integrationsMapper = React.useCallback(
     (recipients: ReadonlyArray<IntegrationRecipient>) => {
-      return recipients.map((r) => {
-        const isDisabled = existingIntegrations?.has(r.integration.id);
+      if ((state.loadingDefault, state.loadingFilter)) {
+        return [
+          <SelectOption key="loading-option">
+            <Skeleton width="100%" />
+          </SelectOption>,
+        ];
+      }
 
+      return recipients.map((recipient, index) => {
+        const isDisabled = existingIntegrations?.has(recipient.integration.id);
+
+        const currRecipient = new RecipientOption(recipient);
         return (
           <SelectOption
-            key={r.getKey()}
-            value={new RecipientOption(r)}
+            key={recipient.integration.id}
+            value={currRecipient}
             description={
               isDisabled ? 'This integration has already been added' : undefined
             }
             isDisabled={isDisabled}
-          />
+            isSelected={selected?.id === recipient.integration.id}
+            isFocused={focusedItemIndex === index}
+            ref={null}
+          >
+            {recipient.displayName}
+          </SelectOption>
         );
       });
     },
-    [existingIntegrations]
+    [
+      existingIntegrations,
+      focusedItemIndex,
+      selected?.id,
+      state.loadingDefault,
+      state.loadingFilter,
+    ]
   );
 
-  const options = useRecipientOptionMemo(state, integrationsMapper);
-
-  const onFilter = React.useCallback(
-    (e: React.ChangeEvent<HTMLInputElement> | null) => {
-      // Ignore filter calls with null event
-      if (e === null) {
-        return options;
-      }
-
-      const search = e.target.value?.trim();
-      if (search === '') {
-        dispatchers.useDefaults();
-      } else {
-        dispatchers.loadFilterValue(search);
-      }
-
-      return options;
-    },
-    [dispatchers, options]
+  const options = integrationsMapper(
+    state.show === 'default' ? state.defaultValues : state.filterValues
   );
 
   const selection = React.useMemo(() => {
-    const sel = props.selected;
+    const sel = selected;
     if (
       sel === undefined ||
       sel.name === undefined ||
@@ -145,41 +152,150 @@ export const IntegrationRecipientTypeahead: React.FunctionComponent<
     }
 
     return new RecipientOption(new IntegrationRecipient(sel as IntegrationRef));
-  }, [props.selected]);
+  }, [selected]);
 
-  const onSelect = React.useCallback(
-    (_event, value: string | SelectOptionObject) => {
-      const integrationSelected = props.onSelected;
-      if (value instanceof RecipientOption) {
-        integrationSelected(value);
-        setOpen(false);
+  const handleMenuArrowKeys = (key: string) => {
+    let indexToFocus;
+
+    if (isOpen) {
+      if (key === 'ArrowUp') {
+        // When no index is set or at the first index, focus to the last, otherwise decrement focus index
+        if (focusedItemIndex === null || focusedItemIndex === 0) {
+          indexToFocus = options.length - 1;
+        } else {
+          indexToFocus = focusedItemIndex - 1;
+        }
       }
-    },
-    [props.onSelected]
+
+      if (key === 'ArrowDown') {
+        // When no index is set or at the last index, focus to the first, otherwise increment focus index
+        if (
+          focusedItemIndex === null ||
+          focusedItemIndex === options.length - 1
+        ) {
+          indexToFocus = 0;
+        } else {
+          indexToFocus = focusedItemIndex + 1;
+        }
+      }
+
+      setFocusedItemIndex(indexToFocus);
+      const focusedItem = options.filter((option) => !option.props.isDisabled)[
+        indexToFocus
+      ];
+      setActiveItem(
+        `select-multi-typeahead-checkbox-${focusedItem.props.value.replace(
+          ' ',
+          '-'
+        )}`
+      );
+    }
+  };
+
+  const onInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const enabledMenuItems = options.filter(
+      (menuItem) => !menuItem.props.isDisabled
+    );
+    const [firstMenuItem] = enabledMenuItems;
+    const focusedItem = focusedItemIndex
+      ? enabledMenuItems[focusedItemIndex]
+      : firstMenuItem;
+
+    switch (event.key) {
+      // Select the first available option
+      case 'Enter':
+        if (!isOpen) {
+          setIsOpen((prevIsOpen) => !prevIsOpen);
+        } else if (isOpen && focusedItem.props.value !== 'no results') {
+          onSelect(focusedItem.props.value);
+        }
+        break;
+      case 'Tab':
+      case 'Escape':
+        setIsOpen(false);
+        setActiveItem(null);
+        break;
+      case 'ArrowUp':
+      case 'ArrowDown':
+        event.preventDefault();
+        handleMenuArrowKeys(event.key);
+        break;
+    }
+  };
+
+  const onToggleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const onTextInputChange = (
+    event: React.FormEvent<HTMLInputElement>,
+    value: string
+  ) => {
+    setInputValue(value);
+
+    const search = event.currentTarget.value?.trim();
+    if (search === '') {
+      dispatchers.useDefaults();
+    } else {
+      dispatchers.loadFilterValue(search);
+    }
+  };
+
+  const onSelect = (value: RecipientOption) => {
+    // eslint-disable-next-line no-console
+
+    if (value) {
+      onSelected(value);
+      setIsOpen(false);
+    }
+
+    setInputValue(value.toString());
+  };
+
+  const toggle = (toggleRef: React.Ref<MenuToggleElement>) => (
+    <MenuToggle
+      variant="typeahead"
+      onClick={onToggleClick}
+      innerRef={toggleRef}
+      isExpanded={isOpen}
+      isFullWidth
+    >
+      <TextInputGroup isPlain>
+        <TextInputGroupMain
+          value={inputValue}
+          onClick={onToggleClick}
+          onChange={onTextInputChange}
+          onKeyDown={onInputKeyDown}
+          id="multi-typeahead-select-checkbox-input"
+          autoComplete="off"
+          innerRef={textInputRef}
+          placeholder={chooseText}
+          {...(activeItem && { 'aria-activedescendant': activeItem })}
+          role="combobox"
+          isExpanded={isOpen}
+          aria-controls="select-multi-typeahead-checkbox-listbox"
+        />
+      </TextInputGroup>
+    </MenuToggle>
   );
 
-  const chooseText = `Choose ${Config.integrations.types[
-    props.integrationType
-  ].name.toLowerCase()}`;
-
   return (
-    <div {...getOuiaProps('IntegrationRecipientTypeahead', props)}>
-      <Select
-        maxHeight={400}
-        variant={SelectVariant.typeahead}
-        typeAheadAriaLabel={chooseText}
-        placeholderText={chooseText}
-        selections={selection}
-        onSelect={onSelect}
-        onToggle={(_e, isOpen) => toggle(isOpen)}
-        isOpen={isOpen}
-        onFilter={onFilter}
-        menuAppendTo={document.body}
-        isDisabled={props.isDisabled}
-        validated={props.error ? 'error' : undefined}
-      >
+    <Select
+      role="menu"
+      id="multi-typeahead-checkbox-select"
+      isOpen={isOpen}
+      selected={selection}
+      onSelect={(_ev, selection) => {
+        if (isRecipientOption(selection)) {
+          onSelect(selection as RecipientOption);
+        }
+      }}
+      onOpenChange={() => setIsOpen(false)}
+      toggle={toggle}
+    >
+      <SelectList id="select-multi-typeahead-checkbox-listbox">
         {options}
-      </Select>
-    </div>
+      </SelectList>
+    </Select>
   );
 };
