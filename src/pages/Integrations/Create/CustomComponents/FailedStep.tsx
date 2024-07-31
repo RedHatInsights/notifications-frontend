67018059a384ff21d68d569c1501cd60fa70a5be
Alexandra Ferdinand <aferdina@redhat.com>
2024-07-31 10:14:51 -0400

    lint fix

--- src/pages/Integrations/Create/CustomComponents/FailedStep.tsx
+++ src/pages/Integrations/Create/CustomComponents/FailedStep.tsx
@@ -9,29 +9,29 @@ interface ProgressProps {
 }
 
 export const FailedStep: React.FunctionComponent<ProgressProps> = (props) => (
-    <>
-      <ErrorState
-        errorTitle="Integration creation failed"
-        errorDescription={
-          <span>
-            There was an error creating <b>&apos;{props.integrationName}</b>
-            &apos; integrations and/or the{' '}
-            <b>&apos;{props.behaviorGroupName}</b>&apos; behavior group.
-          </span>
-        }
-        customFooter={
-          <>
-            <Stack hasGutter>
-              <StackItem>
-                <Button variant="link" onClick={props.onClose}>
-                  Close
-                </Button>
-              </StackItem>
-            </Stack>
-          </>
-        }
-      />
-    </>
-  );
+  <>
+    <ErrorState
+      errorTitle="Integration creation failed"
+      errorDescription={
+        <span>
+          There was an error creating <b>&apos;{props.integrationName}</b>
+          &apos; integrations and/or the <b>&apos;{props.behaviorGroupName}</b>
+          &apos; behavior group.
+        </span>
+      }
+      customFooter={
+        <>
+          <Stack hasGutter>
+            <StackItem>
+              <Button variant="link" onClick={props.onClose}>
+                Close
+              </Button>
+            </StackItem>
+          </Stack>
+        </>
+      }
+    />
+  </>
+);
 
 export default FailedStep;
