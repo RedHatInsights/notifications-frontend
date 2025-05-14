c8d31a1ca72a77315740163fb894c81358da5f83
Justin Orringer <justinorringer@gmail.com>
2025-05-14 09:15:38 -0400

    Cypress config (#700)
    * Cypress config from notif refactor
    
    * Fixes and updating config
    
    * Update test to intercept permissions request
    
    * Removing irrelevant custom commands
    
    * Syncing package-lock

---
+++ cypress/support/commands.ts
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
+
+Cypress.Commands.add('login', () => {
+  cy.session(
+    `login-${Cypress.env('E2E_USER')}`,
+    () => {
+      cy.intercept({ url: '/apps/*', times: 1 }, {});
+      cy.intercept({ url: '/api/', times: 4 }, {});
+      // This JS file causes randomly an uncaught exception on login page which blocks the tests
+      // Cannot read properties of undefined (reading 'setAttribute')
+      cy.visit('/');
+      // disable analytics integrations
+      cy.setLocalStorage('chrome:analytics:disable', 'true');
+      cy.setLocalStorage('chrome:segment:disable', 'true');
+
+      cy.wait(1000);
+      // login into the session
+      try {
+        cy.get('#username-verification').type(Cypress.env('E2E_USER'));
+        cy.get('#login-show-step2').click();
+        cy.get('#password').type(Cypress.env('E2E_PASSWORD'));
+        cy.get('#rh-password-verification-submit-button').click();
+      } catch (error) {
+        cy.get('#username').type(Cypress.env('E2E_USER'));
+        cy.get('#password').type(Cypress.env('E2E_PASSWORD'));
+        cy.get('#submit').click();
+      }
+    },
+    { cacheAcrossSpecs: true }
+  );
+});
